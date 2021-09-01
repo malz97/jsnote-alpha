93d59f1ba94d61c25b3dc65180e9cb8cc00a52c8
Mark Al-Zouibi <mark.alzouibi@rutgers.edu>
2021-09-01 19:25:28 -0400

    Implemented caching layer, refactored plugins

--- packages/local-client/src/components/App.tsx
+++ packages/local-client/src/components/App.tsx
@@ -1,5 +1,7 @@
 import { useState, useEffect, useRef } from 'react';
 import * as esbuild from 'esbuild-wasm';
+import { unpkgPathPlugin } from '../plugins/unpkg-path-plugin';
+import { fetchPlugin } from '../plugins/fetch-plugin';
 
 const App = () => {
   const [input, setInput] = useState('');
@@ -9,16 +11,24 @@ const App = () => {
   const startService = async () => {
     serviceRef.current = await esbuild.startService({
       worker: true,
-      wasmURL: '/esbuild.wasm',
+      wasmURL: 'https://unpkg.com/esbuild-wasm@0.8.27/esbuild.wasm',
     });
   };
   const onClick = async () => {
     if (!serviceRef.current) return;
-    const result = await serviceRef.current.transform(input, {
-      loader: 'jsx',
-      target: 'es2015',
+    const result = await serviceRef.current.build({
+      entryPoints: ['index.js'],
+      bundle: true,
+      write: false,
+      plugins: [unpkgPathPlugin(), fetchPlugin(input)],
+      define: {
+        'process.env.NODE_ENV': '"production"',
+        global: 'window',
+      },
     });
-    setCode(result.code);
+    const { outputFiles } = result;
+    const [data] = outputFiles;
+    setCode(data.text);
   };
 
   useEffect(() => {
---
+++ packages/local-client/src/plugins/unpkg-path-plugin.ts
@@ -0,0 +1,29 @@
+import * as esbuild from 'esbuild-wasm';
+
+export const unpkgPathPlugin = () => {
+  return {
+    name: 'unpkg-path-plugin',
+    setup(build: esbuild.PluginBuild) {
+      // Handle root entry file of 'index.js'
+      build.onResolve({ filter: /(^index*\.js$)/ }, (args: any) => {
+        return { path: args.path, namespace: 'a' };
+      });
+
+      // Handle relative paths in a module
+      build.onResolve({ filter: /^\.+\// }, (args: any) => {
+        return {
+          path: new URL(args.path, `https://unpkg.com${args.resolveDir}/`).href,
+          namespace: 'a',
+        };
+      });
+
+      // Handle main file of module
+      build.onResolve({ filter: /.*/ }, async (args: any) => {
+        return {
+          namespace: 'a',
+          path: `https://unpkg.com/${args.path}`,
+        };
+      });
+    },
+  };
+};
